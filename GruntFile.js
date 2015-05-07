bd181ef01638d60e149741ea6d8773e3aba26fec
Graham Cox <graham@grahamcox.co.uk>
2015-05-07 07:47:43 +0100

    Support for building EsDoc from the source

--- GruntFile.js
+++ GruntFile.js
@@ -1,6 +1,9 @@
+var path = require('path');
+
 var targetDir = 'target';
-var targetMainDir = targetDir + '/main';
-var targetTestDir = targetDir + '/test';
+var targetMainDir = path.join(targetDir, 'main');
+var targetTestDir = path.join(targetDir, 'test');
+var targetDocDir = path.join(targetDir, 'doc');
 
 module.exports = function(grunt) {
     require('jit-grunt')(grunt);
@@ -41,7 +44,27 @@ module.exports = function(grunt) {
                     reporter: 'spec',
                     growl: true
                 },
-                src: [targetTestDir + '/**/*.js']
+                src: [path.join(targetTestDir, '**/*.js')]
+            }
+        },
+        json_generator: {
+            esdoc: {
+                dest: path.join(targetDocDir, 'esdoc.json'),
+                options: {
+                    source: 'src/node/main',
+                    destination: targetDocDir,
+                    access: ['public', 'protected', 'private'],
+                    autoPrivate: true,
+                    unexportIdentifier: true,
+                    undocumentIdentifier: true,
+                    builtinExternal: true,
+                    coverage: true,
+                    test: {
+                        type: 'mocha',
+                        source: 'src/node/test'
+                    },
+                    title: 'Worldbuilder'
+                }
             }
         },
         watch: {
@@ -56,17 +79,27 @@ module.exports = function(grunt) {
         },
         execute: {
             server: {
-                src: targetMainDir + '/index.js'
+                src: path.join(targetMainDir, 'index.js')
+            },
+            esdoc: {
+                src: 'node_modules/.bin/esdoc',
+                options: {
+                    args: [
+                        '-c',
+                        path.join(targetDocDir, 'esdoc.json')
+                    ]
+                }
             }
         }
     });
 
     grunt.registerTask('build', ['babel:server']);
+    grunt.registerTask('doc', ['json_generator:esdoc', 'execute:esdoc']);
 
     grunt.registerTask('run', ['build', 'execute:server']);
     grunt.registerTask('test', ['build', 'babel:test', 'mochaTest:test']);
 
     // Default task(s).
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['build', 'doc', 'test']);
 };
 
