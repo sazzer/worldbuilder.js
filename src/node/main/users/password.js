474e72ca0ad651c5b7a0f032066099ddfb045b0f
Graham Cox <graham@grahamcox.co.uk>
2015-05-08 07:39:34 +0100

    Finished off implementing the password class

--- src/node/main/users/password.js
+++ src/node/main/users/password.js
@@ -1,3 +1,6 @@
+import {v4 as uuid} from 'node-uuid';
+import {createHash} from 'crypto';
+
 /**
  * Representation of a Password that has already been hashed
  */
@@ -35,6 +38,29 @@ export class Password {
      * @return {boolean} True if the passwords match. False if not
      */
     equals(password) {
-        return false;
+        let result;
+        if (password instanceof Password) {
+            result = (password._salt === this._salt && password._hash === this._hash);
+        } else if (typeof password === 'string') {
+            const newHash = hash(password, this._salt);
+            result = (newHash._salt === this._salt && newHash._hash === this._hash);
+        } else {
+            result = false;
+        }
+        return result;
     }
+}
+
+/**
+ * Hash the provided password, either generating the salt or using the provided one
+ * @param {string} password - The password to hash
+ * @param {string} salt - The salt to apply. If undefined then generate some salt
+ * @return {Password} the password
+ */
+export function hash(password, salt = uuid()) {
+    const hashed = createHash('sha256')
+        .update(salt)
+        .update(password)
+        .digest('base64');
+    return new Password(salt, hashed);
 }
--- src/node/test/users/passwordTest.js
+++ src/node/test/users/passwordTest.js
@@ -1,18 +1,80 @@
 import {expect} from 'chai';
-import {Password} from 'users/password';
+import {Password, hash} from 'users/password';
 
 describe('Password', () => {
-    const password = new Password('salt', 'hash');
-    describe('get salt', () => {
-        const salt = password.salt;
-        it('should have the expected value', () => {
-            expect(salt).to.equal("salt");
+    describe('getters', () => {
+        const password = new Password('salt', 'hash');
+        describe('get salt', () => {
+            const salt = password.salt;
+            it('should have the expected value', () => {
+                expect(salt).to.equal("salt");
+            });
+        });
+        describe('get hash', () => {
+            const hash = password.hash;
+            it('should have the expected value', () => {
+                expect(hash).to.equal("hash");
+            });
         });
     });
-    describe('get hash', () => {
-        const hash = password.hash;
-        it('should have the expected value', () => {
-            expect(hash).to.equal("hash");
+
+    describe('hash', () => {
+        describe('When providing a hash', () => {
+            const password = hash('password', 'salt');
+            it('should have the expectedsalt', () => {
+                expect(password.salt).to.equal('salt');
+            });
+            it('should have a hash', () => {
+                expect(password.hash).to.be.defined;
+            });
+            it('the hash should equal this string', () => {
+                expect(password.hash).to.equal('E2Ab2k6njlWge5iGbSvmvgdE44ZvE8AMgRyrYIoo8yI=');
+            });
+        });
+        describe('When generating a hash', () => {
+            const password = hash('password');
+            it('should have a salt', () => {
+                expect(password.salt).to.be.defined;
+            });
+            it('should have a UUID salt', () => {
+                expect(password.salt).to.match(/[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/);
+            });
+            it('should have a hash', () => {
+                expect(password.hash).to.be.defined;
+            });
+            it('the hash should NOT equal the password', () => {
+                expect(password.hash).to.not.equal('password');
+            });
+            it('the hash should NOT equal the salt', () => {
+                expect(password.hash).to.not.equal(password.salt);
+            });
+        });
+    });
+
+    describe('equals', () => {
+        const password = hash('password', 'salt');
+        describe('a Password object', () => {
+            it('should equal an equally hashed password', () => {
+                const password2 = hash('password', 'salt');
+                expect(password.equals(password2)).to.equal(true);
+            });
+            it('should not equal an differently salted hashed password', () => {
+                const password2 = hash('password', 'salted');
+                expect(password.equals(password2)).to.equal(false);
+            });
+            it('should not equal an different hashed password', () => {
+                const password2 = hash('password2', 'salt');
+                expect(password.equals(password2)).to.equal(false);
+            });
+        });
+        describe('a Password string', () => {
+            it('should equal the same password', () => {
+                expect(password.equals('password')).to.equal(true);
+            });
+            it('should not equal a different password', () => {
+                const password2 = hash('password2', 'salt');
+                expect(password.equals('password2')).to.equal(false);
+            });
         });
     });
 });
