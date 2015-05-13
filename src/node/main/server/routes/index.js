cb65e4400c0abd48f058d384360329e3fc822ea8
Graham Cox <graham.cox@oclc.org>
2015-05-13 16:40:18 +0100

    Signature for the method that will load routes

---
+++ src/node/main/server/routes/index.js
@@ -0,0 +1,4 @@
+import {Route} from './route';
+import {loadRoutes} from './loadRoutes';
+
+export {Route, loadRoutes};
---
+++ src/node/main/server/routes/loadRoutes.js
@@ -0,0 +1,10 @@
+/**
+ * Load the routes that are defined in the given file
+ * @param {string} routes - The file to load the routes from
+ * @return {Promise} A promise for the parsed routes
+ */
+export function loadRoutes(routes) {
+    return new Promise((resolve, reject) => {
+        reject();
+    });
+}
