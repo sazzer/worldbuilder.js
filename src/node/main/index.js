21444b5051d166ca643b2ada2ed2954e5df44a44
Graham Cox <graham@grahamcox.co.uk>
2015-05-14 07:57:49 +0100

    Start of streams to load routes file

--- src/node/main/index.js
+++ src/node/main/index.js
@@ -2,6 +2,7 @@ import {config} from 'config';
 import {Server} from 'server/server';
 import {Routes} from 'server/routes/routes';
 import {Route} from 'server/routes/route';
+import {loadRoutes} from 'server/routes';
 
 const routes = new Routes([
     new Route('GET', '/debug/ping', 'server/handlers/debug', 'ping')
@@ -11,5 +12,9 @@ const server = new Server({
     port: config.port,
     routes: routes
 });
+
+loadRoutes('/home/graham/source/worldbuilder.js/src/node/main/routes')
+    .then((routes) => {
+    });
 
 server.run();
--- src/node/main/server/routes/loadRoutes.js
+++ src/node/main/server/routes/loadRoutes.js
@@ -1,10 +1,35 @@
+import {Routes} from './routes';
+import {Route} from './route';
+import {createReadStream} from 'fs';
+import {LineStream} from 'byline';
+import {Writable} from 'stream';
+
 /**
  * Load the routes that are defined in the given file
  * @param {string} routes - The file to load the routes from
  * @return {Promise} A promise for the parsed routes
  */
-export function loadRoutes(routes) {
+export function loadRoutes(routesFile) {
     return new Promise((resolve, reject) => {
-        reject();
+        const routes = [];
+
+        console.log('Loading routes from: ' + routesFile);
+
+        const source = createReadStream(routesFile, {encoding: 'utf8'})
+            .pipe(new LineStream());
+        
+        source.on('error', (e) => {
+            console.log('Error loading routes');
+            reject(e);
+        });
+
+        source.on('finish', () => {
+            console.log('Finished loading routes');
+            resolve(new Routes(routes));
+        });
+
+        source.on('data', (line) => {
+            console.log('===>  ' + line);
+        });
     });
 }
